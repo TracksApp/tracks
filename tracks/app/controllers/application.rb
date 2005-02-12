008d850f13933a1ada2c5407972f735bd4077aa9
bsag <bsag@a4c988fc-2ded-0310-b66e-134b36920a42>
2005-02-12 12:19:59 +0000

    Commented out the line in application.rb which loads iCal.rb.
    git-svn-id: http://www.rousette.org.uk/svn/tracks-repos/trunk@20 a4c988fc-2ded-0310-b66e-134b36920a42

--- tracks/app/controllers/application.rb
+++ tracks/app/controllers/application.rb
@@ -3,7 +3,7 @@
 
 require_dependency "login_system"
 require_dependency "redcloth"
-require_dependency "iCal"
+# require_dependency "iCal"
 require 'date'
 
 $delete_img = "<img src=\"/images/delete.png\" width=\"10\" height=\"10\" border=\"0\" />"
