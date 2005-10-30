f39ebf6ec95fcbf4983cce4aa683e67e15b3bc1c
bsag <bsag@a4c988fc-2ded-0310-b66e-134b36920a42>
2005-10-30 19:12:08 +0000

    Continued the separating out of the new item forms for all the controllers. So the new item form for todo/list, project/show/[name] and context/show/[name] all use shared/add_new_item_form.
    git-svn-id: http://www.rousette.org.uk/svn/tracks-repos/trunk@157 a4c988fc-2ded-0310-b66e-134b36920a42

--- tracks/config/environment.rb
+++ tracks/config/environment.rb
@@ -34,7 +34,7 @@ Rails::Initializer.run do |config|
   
   # Use Active Record's schema dumper instead of SQL when creating the test database
   # (enables use of different database adapters for development and test environments)
-  # config.active_record.schema_format = :ruby
+  config.active_record.schema_format = :ruby
 
   # See Rails::Configuration for more options
 end
