4800172bb4b89f002d21bccaee99036c4293d02c
bsag <bsag@a4c988fc-2ded-0310-b66e-134b36920a42>
2006-02-18 11:03:26 +0000

    Made changes to README_FOR_APP to point out that rake db_schema_import can be used instead of rake migrate with a new, blank database to set it up for use.
    Fixes #210.
    
    
    git-svn-id: http://www.rousette.org.uk/svn/tracks-repos/trunk@186 a4c988fc-2ded-0310-b66e-134b36920a42

--- tracks/app/models/user.rb
+++ tracks/app/models/user.rb
@@ -22,6 +22,9 @@ class User < ActiveRecord::Base
 protected
 
   def self.sha1(pass)
+    # TODO find somewhere else to set the loginhash salt
+    # This is hard-coded for now, but the user needs to be
+    # able to set their own somewhere
     Digest::SHA1.hexdigest("change-me--#{pass}--")
   end
 
--- tracks/db/schema.rb
+++ tracks/db/schema.rb
@@ -5,26 +5,26 @@
 ActiveRecord::Schema.define(:version => 7) do
 
   create_table "contexts", :force => true do |t|
-    t.column "name", :string, :default => "", :null => false
-    t.column "position", :integer, :default => 0, :null => false
+    t.column "name", :string, :null => false
+    t.column "position", :integer, :null => false
     t.column "hide", :boolean, :default => false
-    t.column "user_id", :integer, :default => 0, :null => false
+    t.column "user_id", :integer, :default => 1
   end
 
   create_table "notes", :force => true do |t|
-    t.column "user_id", :integer, :default => 0, :null => false
-    t.column "project_id", :integer, :default => 0, :null => false
+    t.column "user_id", :integer, :null => false
+    t.column "project_id", :integer, :null => false
     t.column "body", :text
     t.column "created_at", :datetime
     t.column "updated_at", :datetime
   end
 
   create_table "projects", :force => true do |t|
-    t.column "name", :string, :default => "", :null => false
-    t.column "position", :integer, :default => 0, :null => false
+    t.column "name", :string, :null => false
+    t.column "position", :integer, :null => false
     t.column "done", :boolean, :default => false
-    t.column "user_id", :integer, :default => 0, :null => false
-    t.column "description", :text
+    t.column "user_id", :integer, :default => 1
+    t.column "description", :text, :default => ""
   end
 
   create_table "sessions", :force => true do |t|
@@ -36,20 +36,20 @@ ActiveRecord::Schema.define(:version => 7) do
   add_index "sessions", ["session_id"], :name => "sessions_session_id_index"
 
   create_table "todos", :force => true do |t|
-    t.column "context_id", :integer, :default => 0, :null => false
+    t.column "context_id", :integer, :null => false
     t.column "project_id", :integer
-    t.column "description", :string, :default => "", :null => false
+    t.column "description", :string, :null => false
     t.column "notes", :text
     t.column "done", :boolean, :default => false, :null => false
     t.column "created_at", :datetime
     t.column "due", :date
     t.column "completed", :datetime
-    t.column "user_id", :integer, :default => 0, :null => false
+    t.column "user_id", :integer, :default => 1
   end
 
   create_table "users", :force => true do |t|
-    t.column "login", :string, :limit => 80, :default => "", :null => false
-    t.column "password", :string, :limit => 40, :default => "", :null => false
+    t.column "login", :string, :limit => 80, :null => false
+    t.column "password", :string, :limit => 40, :null => false
     t.column "word", :string
     t.column "is_admin", :boolean, :default => false, :null => false
     t.column "preferences", :text
