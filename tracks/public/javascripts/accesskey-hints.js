c18db17054056929fa45b69b71f9b2947d0b704d
bsag <bsag@a4c988fc-2ded-0310-b66e-134b36920a42>
2006-03-11 12:19:36 +0000

    Added Luke's patch to re-factor the accesskey-hints.js file using JSON notation.
    Fixes #223.
    
    
    
    git-svn-id: http://www.rousette.org.uk/svn/tracks-repos/trunk@205 a4c988fc-2ded-0310-b66e-134b36920a42

--- tracks/public/javascripts/accesskey-hints.js
+++ tracks/public/javascripts/accesskey-hints.js
@@ -13,78 +13,57 @@
  *  Use the following markup to include the library:
  *  <script type="text/javascript" src="access-key-hints.js"></script>
  */
+var accessKeyHintsAdder = {
+	
+	run : function() {
+	  var elemTypes = new Array('a','area','button','input','label','legend','textarea');
+	  for(var i = 0; i < elemTypes.length; i++)
+	  {
+	    this.addHint(document.getElementsByTagName(elemTypes[i]));
+	  }
+	},
+	
+	addHint : function(elems) {
+	  var elem;
+	  var i = 0;
 
-addEvent(window, "load", addAccessKeyHints);
+	  processElements:
+	  while(elem = elems.item(i++))
+	  {
+	    var accessKey = elem.getAttributeNode("accesskey");
+	    if (!accessKey || !accessKey.value)
+	      continue processElements;
 
-function addAccessKeyHints()
-{
-  var elemTypes = new Array('a','area','button','input','label','legend','textarea');
-  for(var i = 0; i < elemTypes.length; i++)
-  {
-    addHint(document.getElementsByTagName(elemTypes[i]));
-  }
+	    var title = elem.getAttributeNode("title");
+	    if (title && title.value)
+	    {
+	      var overrides = new Array('accesskey','alt+','ctrl+');
+	      for (var j=0; j < overrides.length; j++)
+	      {
+	        if (title.value.toLowerCase().indexOf(overrides[j]) != -1)
+	          continue processElements;
+	      }
+	      elem.setAttribute("title", title.value + ' (' + this.getHintText(accessKey.value) + ')');
+	    }
+	    else
+	    {
+	        elem.setAttribute("title", this.getHintText(accessKey.value));
+	    }
+	  }
+	},
+	
+	getHintText : function(accessKey) {
+        return this.getModifier() + '+' + accessKey.toUpperCase();
+	},
+	
+	getModifier : function() {
+	   var ua = navigator.userAgent.toLowerCase(); 
+	   if (ua.indexOf('mac') == -1)
+	     return 'Alt';
+	   else
+	     return 'Ctrl';
+	}
+	
 }
 
-function addHint(elems)
-{
-  var elem;
-  var i = 0;
-
-  processElements:
-  while(elem = elems.item(i++))
-  {
-    var accessKey = elem.getAttributeNode("accesskey");
-    if (!accessKey || !accessKey.value)
-      continue processElements;
-
-    var title = elem.getAttributeNode("title");
-    if (title && title.value)
-    {
-      var overrides = new Array('accesskey','alt+','ctrl+');
-      for (var j=0; j < overrides.length; j++)
-      {
-        if (title.value.toLowerCase().indexOf(overrides[j]) != -1)
-          continue processElements;
-      }
-      elem.setAttribute("title", title.value + ' (' + getHintText(accessKey.value) + ')');
-    }
-    else
-    {
-        elem.setAttribute("title", getHintText(accessKey.value));
-    }
-  }
-}
-
-function getHintText(accessKey)
-{
-        return getModifier() + '+' + accessKey.toUpperCase();
-}
-
-function getModifier()
-{
-   var ua = navigator.userAgent.toLowerCase(); 
-   if (ua.indexOf('mac') == -1)
-     return 'Alt';
-   else
-     return 'Ctrl';
-}
-
-// This is a cross-browser function for event addition.
-function addEvent(obj, evType, fn)
-{
-  if (obj.addEventListener)
-  {
-    obj.addEventListener(evType, fn, false);
-    return true;
-  }
-  else if (obj.attachEvent)
-  {
-    var r = obj.attachEvent("on" + evType, fn);
-    return r;
-  }
-  else
-  {
-    alert("Event handler could not be attached");
-    return false;
-  }
-}
+Event.observe(window, "load", accessKeyHintsAdder.run.bindAsEventListener(accessKeyHintsAdder));
