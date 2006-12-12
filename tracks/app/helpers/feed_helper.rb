1e4f462f21e7a8692804bd083304b98ad2da78d3
lukemelia <lukemelia@a4c988fc-2ded-0310-b66e-134b36920a42>
2006-12-12 17:35:08 +0000

    Fix problems with feeds not working reported on mailing list.
    git-svn-id: http://www.rousette.org.uk/svn/tracks-repos/trunk@375 a4c988fc-2ded-0310-b66e-134b36920a42

--- tracks/app/helpers/feed_helper.rb
+++ tracks/app/helpers/feed_helper.rb
@@ -68,19 +68,19 @@ module FeedHelper
   
   def rss_feed_link(options = {})
     image_tag = image_tag("feed-icon.png", :size => "16X16", :border => 0, :class => "rss-icon")
-    linkoptions = {:controller => 'feed', :action => 'rss', :name => "#{@user.login}", :token => "#{@user.word}"}
+    linkoptions = {:controller => 'feed', :action => 'rss', :login => "#{@user.login}", :token => "#{@user.word}"}
     linkoptions.merge!(options)
 		link_to(image_tag, linkoptions, :title => "RSS feed")
   end
   
   def text_feed_link(options = {})
-    linkoptions = {:controller => 'feed', :action => 'text', :name => "#{@user.login}", :token => "#{@user.word}"}
+    linkoptions = {:controller => 'feed', :action => 'text', :login => "#{@user.login}", :token => "#{@user.word}"}
     linkoptions.merge!(options)
     link_to('<span class="feed">TXT</span>', linkoptions, :title => "Plain text feed" )
   end
   
   def ical_feed_link(options = {})
-    linkoptions = {:controller => 'feed', :action => 'ical', :name => "#{@user.login}", :token => "#{@user.word}"}
+    linkoptions = {:controller => 'feed', :action => 'ical', :login => "#{@user.login}", :token => "#{@user.word}"}
     linkoptions.merge!(options)
     link_to('<span class="feed">iCal</span>', linkoptions, :title => "iCal feed")
   end
