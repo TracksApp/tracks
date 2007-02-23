0a28edb2d83d190f4ba0c32ba530cf47f12ad4d3
lukemelia <lukemelia@a4c988fc-2ded-0310-b66e-134b36920a42>
2007-02-23 04:31:49 +0000

    Remove transactions from migrations to support sqlite-3. Fixes #451.
    git-svn-id: http://www.rousette.org.uk/svn/tracks-repos/trunk@453 a4c988fc-2ded-0310-b66e-134b36920a42

--- tracks/db/migrate/019_convert_todo_to_state_machine.rb
+++ tracks/db/migrate/019_convert_todo_to_state_machine.rb
@@ -6,39 +6,35 @@ class ConvertTodoToStateMachine < ActiveRecord::Migration
   class Project < ActiveRecord::Base; end
 
   def self.up
-    ActiveRecord::Base.transaction do
-      add_column :todos, :state, :string, :limit => 20, :default => "immediate", :null => false
-      @todos = Todo.find(:all)
-      @todos.each do |todo|
-        if todo.done?
-          todo.state = 'completed'
-        elsif todo.type == 'Deferred'
-          todo.state = 'deferred'
-        elsif !todo.project.nil? && todo.project.state == 'hidden'
-          todo.state = 'project_hidden'
-        else
-          todo.state = 'active'
-        end
-        todo.save
+    add_column :todos, :state, :string, :limit => 20, :default => "immediate", :null => false
+    @todos = Todo.find(:all)
+    @todos.each do |todo|
+      if todo.done?
+        todo.state = 'completed'
+      elsif todo.type == 'Deferred'
+        todo.state = 'deferred'
+      elsif !todo.project.nil? && todo.project.state == 'hidden'
+        todo.state = 'project_hidden'
+      else
+        todo.state = 'active'
       end
-      rename_column :todos, :completed, :completed_at
-      remove_column :todos, :done
-      remove_column :todos, :type
+      todo.save
     end
+    rename_column :todos, :completed, :completed_at
+    remove_column :todos, :done
+    remove_column :todos, :type
   end
 
   def self.down
-    ActiveRecord::Base.transaction do
-      add_column :todos, :done, :integer, :limit => 4, :default => 0, :null => false
-      add_column :todos, :type, :string, :default => "Immediate", :null => false
-      rename_column :todos, :completed_at, :completed
-      @todos = Todo.find(:all)
-      @todos.each do |todo|
-        todo.done = todo.state == 'completed'
-        todo.type = todo.type == 'deferred' ? 'Deferred' : 'Immediate'
-        todo.save
-      end
-      remove_column :todos, :state
+    add_column :todos, :done, :integer, :limit => 4, :default => 0, :null => false
+    add_column :todos, :type, :string, :default => "Immediate", :null => false
+    rename_column :todos, :completed_at, :completed
+    @todos = Todo.find(:all)
+    @todos.each do |todo|
+      todo.done = todo.state == 'completed'
+      todo.type = todo.type == 'deferred' ? 'Deferred' : 'Immediate'
+      todo.save
     end
+    remove_column :todos, :state
   end
 end
