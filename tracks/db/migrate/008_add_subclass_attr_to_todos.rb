3dcc0152a1c6ec5933a974fae6bef2133bf40e54
lukemelia <lukemelia@a4c988fc-2ded-0310-b66e-134b36920a42>
2007-03-25 02:20:10 +0000

    Apply James Kebinger's patch to fix #407. Thanks, James!
    git-svn-id: http://www.rousette.org.uk/svn/tracks-repos/trunk@483 a4c988fc-2ded-0310-b66e-134b36920a42

--- tracks/db/migrate/008_add_subclass_attr_to_todos.rb
+++ tracks/db/migrate/008_add_subclass_attr_to_todos.rb
@@ -1,7 +1,8 @@
 class AddSubclassAttrToTodos < ActiveRecord::Migration
 
   class Todo < ActiveRecord::Base; end
-
+  class Immediate < Todo; end
+  
   def self.up
     add_column :todos, :type, :string, :null => false, :default => "Immediate"
     add_column :todos, :show_from, :date
