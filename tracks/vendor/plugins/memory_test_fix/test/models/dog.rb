0d8a3fcf6d40c293ee1f4c7eb5ba0ac315067d2a
lukemelia <lukemelia@a4c988fc-2ded-0310-b66e-134b36920a42>
2007-03-30 12:34:49 +0000

    copy memory_test_fix plugin from branch
    git-svn-id: http://www.rousette.org.uk/svn/tracks-repos/trunk@509 a4c988fc-2ded-0310-b66e-134b36920a42

---
+++ tracks/vendor/plugins/memory_test_fix/test/models/dog.rb
@@ -0,0 +1,16 @@
+class Dog < ActiveRecord::Base
+  attr_accessor :after_find_test, :after_initialize_test
+
+  set_table_name "bow_wows"
+  
+  def after_find
+    @after_find_test = true
+#    puts "After find called on #{name}."
+  end
+  
+  def after_initialize
+    @after_initialize_test = true
+  end
+  
+end
+
