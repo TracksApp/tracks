a6501c3d83d4866b84264eac7b0ff649235a832f
lukemelia <lukemelia@a4c988fc-2ded-0310-b66e-134b36920a42>
2007-12-11 04:33:31 +0000

    Preparing to upgrade has_many_polymorphs plugin...
    git-svn-id: http://www.rousette.org.uk/svn/tracks-repos/trunk@682 a4c988fc-2ded-0310-b66e-134b36920a42

--- tracks/vendor/plugins/has_many_polymorphs/install.rb
+++
@@ -1,1 +0,0 @@
-puts open("#{File.dirname(__FILE__)}/TAGGING_INSTALL").read
--- tracks/vendor/plugins/has_many_polymorphs/test/models/aquatic/fish.rb
+++
@@ -1,4 +0,0 @@
-class Aquatic::Fish < ActiveRecord::Base
-#  attr_accessor :after_find_test, :after_initialize_test
-end
-
