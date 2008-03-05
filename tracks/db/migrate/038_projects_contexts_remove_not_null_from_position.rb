e71a3cbbae1bf0c156e26778cf07e6456e9a38ea
lrbalt <lrbalt@a4c988fc-2ded-0310-b66e-134b36920a42>
2008-03-05 08:49:24 +0000

    restored :integer as type for position. I've updated migration 038. If you have run it before this patch, you need to rerun it. rake db:migratie VERSION=37 to step back and rake db:migrate to rerun
    git-svn-id: http://www.rousette.org.uk/svn/tracks-repos/trunk@729 a4c988fc-2ded-0310-b66e-134b36920a42

--- tracks/db/migrate/038_projects_contexts_remove_not_null_from_position.rb
+++ tracks/db/migrate/038_projects_contexts_remove_not_null_from_position.rb
@@ -1,7 +1,7 @@
 class ProjectsContextsRemoveNotNullFromPosition < ActiveRecord::Migration
   def self.up
-    change_column :projects, :position, :string, :null => true
-    change_column :contexts, :position, :string, :null => true
+    change_column :projects, :position, :integer, {:null => true, :default => false}
+    change_column :contexts, :position, :integer, {:null => true, :default => false}
   end
 
   def self.down
@@ -10,13 +10,13 @@ class ProjectsContextsRemoveNotNullFromPosition < ActiveRecord::Migration
       project.position = 0 if !project.position?
       project.save
     end
-    change_column :projects, :position, :string, :null => false
+    change_column :projects, :position, :integer, {:null => false, :default => false}
 
     @contexts = Context.find(:all)
     @contexts.each do |context|
       context.position = 0 if !context.position?
       context.save
     end
-    change_column :contexts, :position, :string, :null => false
+    change_column :contexts, :position, :integer, {:null => false, :default => false}
   end
 end
