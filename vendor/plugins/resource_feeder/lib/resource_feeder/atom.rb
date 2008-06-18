85dc6f48987024b9421dd9db346c87ae1878f782
Luke Melia <luke@lukemelia.com>
2008-06-18 16:13:37 -0400

    Removed SimplyHelpful plugin (functionality has been rolled into Rails) and modified ResourceFeeder as necessary.

--- vendor/plugins/resource_feeder/lib/resource_feeder/atom.rb
+++ vendor/plugins/resource_feeder/lib/resource_feeder/atom.rb
@@ -3,6 +3,7 @@ require 'resource_feeder/common'
 module ResourceFeeder
   module Atom
     include ResourceFeeder::Common
+    include ActionController::Routing
     extend self
     
     def render_atom_feed_for(resources, options = {})
@@ -25,14 +26,14 @@ module ResourceFeeder
       
       options[:feed][:title] ||= klass.name.pluralize
       options[:feed][:id]    ||= "tag:#{request.host_with_port}:#{klass.name.pluralize}"
-      options[:feed][:link]  ||= SimplyHelpful::PolymorphicRoutes.polymorphic_url(new_record, options[:url_writer])
+      options[:feed][:link]  ||= polymorphic_url(new_record, :controller => options[:url_writer])
       
       options[:item][:title]       ||= [ :title, :subject, :headline, :name ]
       options[:item][:description] ||= [ :description, :body, :content ]
       options[:item][:pub_date]    ||= [ :updated_at, :updated_on, :created_at, :created_on ]
       options[:item][:author]      ||= [ :author, :creator ]
       
-      resource_link = lambda { |r| SimplyHelpful::PolymorphicRoutes.polymorphic_url(r, options[:url_writer]) }
+      resource_link = lambda { |r| polymorphic_url(r, :controller => options[:url_writer]) }
 
       xml.instruct!
       xml.feed "xml:lang" => "en-US", "xmlns" => 'http://www.w3.org/2005/Atom' do
