d47e863dfc87e995faec9bb96397ceecda24585a
Luke Melia <luke@lukemelia.com>
2008-06-19 00:14:04 -0400

    Started moving selenium on rails tests over to RSpec stories. See the updated README_DEVELOPERS for info on running the new tests.

--- stories/steps/login.rb
+++ stories/steps/login.rb
@@ -1,19 +1,6 @@
-steps_for :login do  
-  Given "an admin user Reinier with the password abracadabra" do
-    @reinier = User.create!(:login => 'reinier', :password => 'abracadabra', :password_confirmation => 'abracadabra', :is_admin => true)
-    @reinier.create_preference
-  end
-  
-  Given "Reinier is not logged in" do
-  end
-
-  Given "no users exist" do
-    User.delete_all
-  end
-  
-  Given "a visitor named Reinier" do
-  end
-  
+steps_for :login do
+  include_steps_for :users
+    
   When "Reinier submits the login form with an incorrect password" do
     fills_in 'Login', :with => 'reinier'
     fills_in 'Password', :with => 'incorrectpass'
