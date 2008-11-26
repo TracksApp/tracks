764e41685beb8a42c5862b615ea8fe87a10a3869
Reinier Balt <lrbalt@gmail.com>
2008-11-26 10:53:00 +0100

    fix migration to fill the updated_at column. The rss feeds need a filled column. Fixes #794

--- db/migrate/043_add_updated_at_to_todos.rb
+++ db/migrate/043_add_updated_at_to_todos.rb
@@ -1,8 +1,10 @@
 class AddUpdatedAtToTodos < ActiveRecord::Migration
   def self.up
       add_column :todos, :updated_at, :timestamp
-        end
-          def self.down
-              remove_column :todos, :updated_at
-                end
-                end
+      execute 'update todos set updated_at = created_at where completed_at IS NULL'
+      execute 'update todos set updated_at = completed_at where NOT completed_at IS NULL'
+  end
+  def self.down
+      remove_column :todos, :updated_at
+  end
+end
