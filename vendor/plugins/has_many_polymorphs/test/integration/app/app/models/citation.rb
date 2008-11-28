56242fefa6d0f07b69498b6901d301ea09b465da
Reinier Balt <lrbalt@gmail.com>
2008-11-28 08:36:43 +0100

    remove plugin had_many_polymorphs

--- vendor/plugins/has_many_polymorphs/test/integration/app/app/models/citation.rb
+++
@@ -1,3 +0,0 @@
-class Citation < ActiveRecord::Base
-  has_many_polymorphs :items, :from => [:users, :sellers]
-end
--- vendor/plugins/has_many_polymorphs/test/integration/app/app/models/seller.rb
+++
@@ -1,4 +0,0 @@
-class Seller < ActiveRecord::Base
-  belongs_to :user  
-  delegate :address, :to => :user
-end
