5ee939ac47141587d8f12863bb7ba07d436e538d
Vitalie Lazu <vitalie.lazu@gmail.com>
2009-01-11 17:57:07 +0800

    Fixed exception in preference model when todo xml api is used with show_from attribute
    Signed-off-by: bsag <butshesagirl@rousette.org.uk>

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,28 +1,37 @@
 class Preference < ActiveRecord::Base
   belongs_to :user
   belongs_to :sms_context, :class_name => 'Context'
-    
+
   def self.due_styles
     { :due_in_n_days => 0, :due_on => 1}
   end
-           
+
   def self.day_number_to_name_map
     { 0 => "Sunday",
-		  1 => "Monday",
-			2 => "Tuesday",
-			3 => "Wednesday",
-			4 => "Thursday",
-			5 => "Friday",
-			6 => "Saturday"}
+                  1 => "Monday",
+                        2 => "Tuesday",
+                        3 => "Wednesday",
+                        4 => "Thursday",
+                        5 => "Friday",
+                        6 => "Saturday"}
   end
-  
+
   def hide_completed_actions?
     return show_number_completed == 0
   end
-  
+
   def parse_date(s)
     return nil if s.blank?
-    user.at_midnight(Date.strptime(s, date_format))
+    date = nil
+
+    if s.is_a?(Time)
+      date = s.to_date
+    elsif s.is_a?(String)
+      date = Date.strptime(s, date_format)
+    else
+      raise ArgumentError.new("Bad argument type:#{s.class}")
+    end
+
+    user.at_midnight(date)
   end
-  
 end
