3554dd8dd01a310793d425f6fb9e2b2bae811278
bsag <butshesagirl@rousette.org.uk>
2009-01-11 15:48:57 +0000

    Tidied formatting in app/model/preference.rb

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -8,12 +8,12 @@ class Preference < ActiveRecord::Base
 
   def self.day_number_to_name_map
     { 0 => "Sunday",
-                  1 => "Monday",
-                        2 => "Tuesday",
-                        3 => "Wednesday",
-                        4 => "Thursday",
-                        5 => "Friday",
-                        6 => "Saturday"}
+      1 => "Monday",
+      2 => "Tuesday",
+      3 => "Wednesday",
+      4 => "Thursday",
+      5 => "Friday",
+      6 => "Saturday"}
   end
 
   def hide_completed_actions?
