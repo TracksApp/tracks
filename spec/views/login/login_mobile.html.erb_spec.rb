96762630f2e6e5c352764bb335f65f20058849f4
Eric Allen <ericpallen@gmail.com>
2009-05-03 23:53:40 -0400

    Fix failing specs. All specs and tests pass now.

--- spec/views/login/login_mobile.html.erb_spec.rb
+++ spec/views/login/login_mobile.html.erb_spec.rb
@@ -1,7 +1,8 @@
 require File.dirname(__FILE__) + '/../../spec_helper'
 
-describe "/login.m" do
+describe "/login/login_mobile.html.erb" do
   it "should render without an error" do
+    @controller.template.stub!(:set_default_external!)
     render :action => 'login/login_mobile.html.erb', :layout => 'mobile.m.erb'
     response.should_not have_tag("div#Application-Trace")
   end
