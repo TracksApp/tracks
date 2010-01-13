7a92d3559b9496ec753079321eccd5f0111bbc62
Reinier Balt <lrbalt@gmail.com>
2010-01-13 21:52:04 +0100

    get all cucumber stories running for default profile

--- features/step_definitions/generic_steps.rb
+++ features/step_definitions/generic_steps.rb
@@ -1,4 +1,4 @@
-Then "the badge should show (.*)" do |number|
+Then /the badge should show (.*)/ do |number|
   badge = -1
   response.should have_xpath("//span[@id='badge_count']") do |node|
     badge = node.first.content.to_i
