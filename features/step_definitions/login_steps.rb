f53b386bd1d0a58237c220fcb712c7863bec8a2b
Reinier Balt <lrbalt@gmail.com>
2010-05-05 13:15:00 +0200

    remove anoying message about login text not found when running cucumber/selenium

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -3,6 +3,10 @@ Given /^I have logged in as "(.*)" with password "(.*)"$/ do |username, password
   fill_in "Login", :with => username
   fill_in "Password", :with => password
   click_button
+  selenium.wait_for_page_to_load(5000)
+#  wait_for do
+#    selenium.is_visible("flash")
+#  end
   response.should contain(/Login successful/)
   @current_user = User.find_by_login(username)
 end
