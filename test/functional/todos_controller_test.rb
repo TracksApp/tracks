b50a1ce26f5287d82ed6a5fbc51ee2c304f512b9
Eric Allen <ericpallen@gmail.com>
2010-05-17 11:02:23 -0400

    Fix a couple of test definitions to avoid false test failures
    State should be unspecified to default to active
    
    Fixes #977 (again)

--- test/functional/todos_controller_test.rb
+++ test/functional/todos_controller_test.rb
@@ -320,7 +320,7 @@ class TodosControllerTest < ActionController::TestCase
         "due(1i)"=>"2007", "due(2i)"=>"1", "due(3i)"=>"2",
         "show_from(1i)"=>"", "show_from(2i)"=>"", "show_from(3i)"=>"",
         "project_id"=>"1",
-        "notes"=>"test notes", "description"=>"test_mobile_create_action", "state"=>"0"}}
+        "notes"=>"test notes", "description"=>"test_mobile_create_action"}}
     t = Todo.find_by_description("test_mobile_create_action")
     assert_not_nil t
     assert_equal 2, t.context_id
@@ -347,7 +347,7 @@ class TodosControllerTest < ActionController::TestCase
         "due(1i)"=>"2007", "due(2i)"=>"1", "due(3i)"=>"2",
         "show_from(1i)"=>"", "show_from(2i)"=>"", "show_from(3i)"=>"",
         "project_id"=>"1",
-        "notes"=>"test notes", "state"=>"0"}, "tag_list"=>"test, test2"}
+        "notes"=>"test notes"}, "tag_list"=>"test, test2"}
     assert_template 'todos/new'
   end
 
