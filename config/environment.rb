012a5929e1a4be6a8217a3ec6f9fa554a8009500
Reinier Balt <lrbalt@gmail.com>
2010-09-28 17:29:17 +0200

    rename load_path to autoload_path to remove deprecation warning

--- config/environment.rb
+++ config/environment.rb
@@ -19,7 +19,7 @@ Encoding.default_external = Encoding::UTF_8 if RUBY_VERSION > "1.9"
 Rails::Initializer.run do |config|
   # Skip frameworks you're not going to use
   # config.frameworks -= [ :action_web_service, :action_mailer ]
-  config.load_paths += %W( #{RAILS_ROOT}/app/apis )
+  config.autoload_paths += %W( #{RAILS_ROOT}/app/apis )
 
   config.gem "highline"
   config.gem "RedCloth"
