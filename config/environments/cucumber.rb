ea4f7de8131c1fb9168c8224b72595a5eb938b31
Reinier Balt <lrbalt@gmail.com>
2011-02-04 20:21:08 +0100

    get cucumber running again

--- config/environments/cucumber.rb
+++ config/environments/cucumber.rb
@@ -21,6 +21,11 @@ config.action_controller.allow_forgery_protection    = false
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
 
+# Unique cookies
+# Unique cookies and use cookies for session
+config.action_controller.session_store = :cookie_store
+config.action_controller.session = { :key => 'TracksCucumber', :secret => SITE_CONFIG['salt'] * (30.0 /  SITE_CONFIG['salt'].length).ceil }
+
 config.gem 'cucumber',         :lib => false, :version => '<0.10.0' unless File.directory?(File.join(Rails.root, 'vendor/plugins/cucumber'))
 config.gem 'cucumber-rails',   :lib => false, :version => '>=0.3.2' unless File.directory?(File.join(Rails.root, 'vendor/plugins/cucumber-rails'))
 config.gem 'gherkin',          :lib => false, :version => '2.2.9'   unless File.directory?(File.join(Rails.root, 'vendor/plugins/gherkin'))
