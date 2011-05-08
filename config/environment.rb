22a0c940fdaa9f30da81d828891273c9834de01e
Reinier Balt <lrbalt@gmail.com>
2011-05-09 01:30:05 +0200

    bump version number

--- config/environment.rb
+++ config/environment.rb
@@ -121,7 +121,7 @@ if ( SITE_CONFIG['authentication_schemes'].include? 'cas')
   end
 end
 
-tracks_version='2.0RC3'
+tracks_version='2.0'
 # comment out next two lines if you do not want (or can not) the date of the
 # last git commit in the footer
 #info=`git log --pretty=format:"%ai" -1`
