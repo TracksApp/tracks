fdbd2a6391848c8131444bffc483c137fa951254
Stefan Richter <stefan@02strich.de>
2011-05-27 03:44:32 +0800

    fixing issue with wrong initial value of state column

---
+++ db/migrate/20110526192008_adapt_to_new_aasm.rb
@@ -0,0 +1,11 @@
+class AdaptToNewAasm < ActiveRecord::Migration
+  def self.up
+    change_column :todos, :state, :string, :limit => 20, :default => "", :null => false
+    change_column :projects, :state, :string, :limit => 20, :default => "", :null => false
+  end
+
+  def self.down
+    change_column :todos, :state, :string, :limit => 20, :default => "immediate", :null => false
+    change_column :projects, :state, :string, :limit => 20, :default => "active", :null => false
+  end
+end
