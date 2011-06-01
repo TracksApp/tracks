1cf5967eb5ee85e30ce7fbc79e8c39778ae840e5
Stefan Richter <stefan@02strich.de>
2011-06-02 03:05:05 +0800

    adding missing table

--- db/migrate/20110526192008_adapt_to_new_aasm.rb
+++ db/migrate/20110526192008_adapt_to_new_aasm.rb
@@ -1,11 +1,13 @@
 class AdaptToNewAasm < ActiveRecord::Migration
   def self.up
-    change_column :todos, :state, :string, :limit => 20, :default => "", :null => false
-    change_column :projects, :state, :string, :limit => 20, :default => "", :null => false
+    change_column :todos,           :state, :string, :limit => 20, :default => "", :null => false
+    change_column :projects,        :state, :string, :limit => 20, :default => "", :null => false
+    change_column :recurring_todos, :state, :string, :limit => 20, :default => "", :null => false
   end
 
   def self.down
-    change_column :todos, :state, :string, :limit => 20, :default => "immediate", :null => false
-    change_column :projects, :state, :string, :limit => 20, :default => "active", :null => false
+    change_column :todos,           :state, :string, :limit => 20, :default => "immediate", :null => false
+    change_column :projects,        :state, :string, :limit => 20, :default => "active", :null => false
+    change_column :recurring_todos, :state, :string, :limit => 20, :default => "active", :null => false
   end
 end
