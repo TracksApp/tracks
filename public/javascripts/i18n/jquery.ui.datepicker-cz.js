4931a326e7fa57eccc8cc85dba627add9d7d2659
Pavel Župa <pavel.zupa@gmail.com>
2011-12-08 12:05:54 +0100

    slight modifications to czech translation

--- public/javascripts/i18n/jquery.ui.datepicker-cz.js
+++ public/javascripts/i18n/jquery.ui.datepicker-cz.js
@@ -1,5 +1,5 @@
-﻿/* German initialisation for the jQuery UI date picker plugin. */
-/* Written by Milian Wolff (mail@milianw.de). */
+﻿/* Czech initialisation for the jQuery UI date picker plugin. */
+/* Written by Pavel Župa (pavel.zupa@gmail.com). */
 jQuery(function($){
 	$.datepicker.regional['cz'] = {
 		closeText: 'zavřít',
@@ -8,7 +8,7 @@ jQuery(function($){
 		currentText: 'dnes',
 		monthNames: ['Leden','Únor','Březen','Duben','Květen','Červen',
 		'Červenec','Srpen','Září','Říjen','Listopad','Prosinec'],
-		monthNamesShort: ['Led','Úno','Bře','Dub','Kvě','Čvn',
+		monthNamesShort: ['Led','Úno','Bře','Dub','Kvě','Čer',
 		'Čec','Srp','Zář','Říj','Lis','Pro'],
 		dayNames: ['Neděle','Pondělí','Úterý','Středa','Čtvrtek','Pátek','Sobota'],
 		dayNamesShort: ['Ne','Po','Út','St','Čt','Pá','So'],
