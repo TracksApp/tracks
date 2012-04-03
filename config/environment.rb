0479c7b81cf874fb8c22970ec68655e8a4871184
Reinier Balt <lrbalt@gmail.com>
2012-04-03 14:41:57 +0200

    Updating for RC1 release

--- config/environment.rb
+++ config/environment.rb
@@ -110,10 +110,10 @@ end
 # changed in development.rb to show under_construction bar
 NOTIFY_BAR = "" unless defined?(NOTIFY_BAR)
 
-tracks_version='2.1devel'
+tracks_version='2.1RC1'
 # comment out next two lines if you do not want (or can not) the date of the
 # last git commit in the footer
-info=`git log --pretty=format:"%ai" -1`
-tracks_version=tracks_version + ' ('+info+')'
+# info=`git log --pretty=format:"%ai" -1`
+# tracks_version=tracks_version + ' ('+info+')'
 
 TRACKS_VERSION=tracks_version
