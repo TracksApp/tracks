b20f09725cd9c103474cffa91b43034d92d57d4e
Reinier Balt <lrbalt@gmail.com>
2012-05-18 17:51:50 +0200

    do not depend on ruby 1.9.x for production use

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
+if RUBY_VERSION.to_f >= 1.9
+  Encoding.default_external = Encoding::UTF_8
+  Encoding.default_internal = Encoding::UTF_8
+end
 
 # Initialize the rails application
 Tracksapp::Application.initialize!
