5d703975a34403da41fe5b9426d9e2cfab8a6946
Reinier Balt <lrbalt@gmail.com>
2012-06-27 14:07:57 +0200

    fix #1292. Make the sort order in project view the same as context view

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ActiveRecord::Base
-  has_many :todos, :dependent => :delete_all
+  has_many :todos, :dependent => :delete_all, :order => 'todos.due IS NULL, todos.due ASC, todos.created_at ASC'
   has_many :notes, :dependent => :delete_all, :order => "created_at DESC"
   has_many :recurring_todos
 
