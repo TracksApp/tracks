9af3dee277bed75c189766b17fe64981e62329eb
Reinier Balt <lrbalt@gmail.com>
2012-07-12 11:17:08 +0200

    remove backup files from rails2 - rails3 migration

--- backup.rails2.3/lib/name_part_finder.rb
+++
@@ -1,5 +0,0 @@
-module NamePartFinder
-  def find_by_namepart(namepart)
-    find_by_name(namepart) || find(:first, :conditions => ["name LIKE ?", namepart + '%'])
-  end
-end
--- backup.rails2.3/plugins/simple_ldap_authenticator/init.rb
+++
@@ -1,2 +0,0 @@
-# Include hook code here
-#require 'simple_ldap_authenticator'
