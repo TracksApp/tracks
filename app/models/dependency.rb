b3059cf53ac5949a319a957d9f4b400fb244e9c6
Reinier Balt <lrbalt@gmail.com>
2012-09-19 17:13:29 +0200

    fix caching for #1328 and #1329

--- app/models/dependency.rb
+++ app/models/dependency.rb
@@ -1,7 +1,9 @@
 class Dependency < ActiveRecord::Base
 
-  belongs_to :predecessor, :foreign_key => 'predecessor_id', :class_name => 'Todo'
-  belongs_to :successor,   :foreign_key => 'successor_id',   :class_name => 'Todo'
+  # touch to make sure todo caches for predecessor and successor are invalidated
+  
+  belongs_to :predecessor, :foreign_key => 'predecessor_id', :class_name => 'Todo', :touch => true
+  belongs_to :successor,   :foreign_key => 'successor_id',   :class_name => 'Todo', :touch => true
   
 end
 
