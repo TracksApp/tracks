c1f1fcb35bbb00a6d09c376251dc47d50ec4fab5
Dan Rice <drice@diego>
2012-12-04 19:04:01 -0500

    Use say() instead of puts() in migrations

--- db/migrate/20120412072508_add_rendered_notes.rb
+++ db/migrate/20120412072508_add_rendered_notes.rb
@@ -4,10 +4,10 @@ class AddRenderedNotes < ActiveRecord::Migration
     
     Todo.reset_column_information
 
-    puts "-- Clearing show_from dates from completed todos"
+    say "Clearing show_from dates from completed todos"
     # clear up completed todos that have show_from set. These could have been left over from before the AASM migration
     Todo.completed.find(:all, :conditions =>[ "NOT(show_from IS NULL)"]).each {|t| t.show_from=nil; t.save!}
-    puts "-- Generating new column values from notes. This may take a while."
+    say "Generating new column values from notes. This may take a while."
     # Call save! on each todo to force generation of rendered_todos
     i=0; max = Todo.all.count; start = Time.now
     Todo.all.each do |todo|
@@ -16,10 +16,10 @@ class AddRenderedNotes < ActiveRecord::Migration
       if i%250==0
         elapsed_sec = (Time.now-start)
         remaining = (elapsed_sec / i)*(max-i)
-        puts "Progress: #{i} / #{max} (#{(i.to_f/max.to_f*100.0).floor}%) ETA=#{remaining.floor}s"
+        say "Progress: #{i} / #{max} (#{(i.to_f/max.to_f*100.0).floor}%) ETA=#{remaining.floor}s"
       end
     end
-    puts "Done: #{i} / #{max}"
+    say "Done: #{i} / #{max}"
   end
 
   def self.down
