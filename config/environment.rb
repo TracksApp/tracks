37f1926a0c9ed40caab1c405499aeda76b26332e
Matt Rogers <mattrogers@sbcglobal.net>
2013-01-29 16:33:32 -0600

    Bump version to 2.1.3

--- config/environment.rb
+++ config/environment.rb
@@ -110,7 +110,7 @@ end
 # changed in development.rb to show under_construction bar
 NOTIFY_BAR = "" unless defined?(NOTIFY_BAR)
 
-tracks_version='2.1.2'
+tracks_version='2.1.3'
 # comment out next two lines if you do not want (or can not) the date of the
 # last git commit in the footer
 # info=`git log --pretty=format:"%ai" -1`
