8dfbac0fe83964a4f83e84f5b8a16fc8f219d49c
Matt Rogers <mattrogers@sbcglobal.net>
2013-03-09 22:29:13 -0600

    Use the newly extracted method object for getting todo staleness

--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -173,17 +173,7 @@ module TodosHelper
   # * l3: created more than 3 x staleness_starts
   #
   def staleness_class(item)
-    if item.due || item.completed?
-      return ""
-    elsif item.created_at < current_user.time - (prefs.staleness_starts * 3).days
-      return " stale_l3"
-    elsif item.created_at < current_user.time - (prefs.staleness_starts * 2).days
-      return " stale_l2"
-    elsif item.created_at < current_user.time - (prefs.staleness_starts).days
-      return " stale_l1"
-    else
-      return ""
-    end
+    Staleness.days_stale(item, current_user)
   end
 
   def show_date_tag(date, the_class, text)
