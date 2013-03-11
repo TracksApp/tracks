053e3fc8d69b974fa6ff81b17ae225470d38c336
Matt Rogers <mattrogers@sbcglobal.net>
2013-03-10 22:48:27 -0500

    Guard against the case days stale can be negative

--- test/unit/staleness_test.rb
+++ test/unit/staleness_test.rb
@@ -14,6 +14,10 @@ class StalenessTest < Test::Unit::TestCase
     @now ||= Time.utc(2013, 2, 28, 0, 0, 0)
   end
 
+  def after_now
+    @after_now ||= Time.utc(2013, 3, 1, 0, 0, 0)
+  end
+
   def day16
     @day16 ||= Time.utc(2013, 2, 12, 0, 0, 0)
   end
@@ -33,6 +37,11 @@ class StalenessTest < Test::Unit::TestCase
 
   def test_complete_item_is_not_stale
     todo = FakeTask.new(day8, true, day16)
+    assert_equal 0, Staleness.days_stale(todo, @current_user)
+  end
+
+  def test_created_at_after_current_time_is_not_stale
+    todo = FakeTask.new(nil, false, after_now)
     assert_equal 0, Staleness.days_stale(todo, @current_user)
   end
 
