83273ac34220e81849e741a2ebfc663708932ddd
Matt Rogers <mattrogers@sbcglobal.net>
2013-04-30 20:19:27 -0500

    add the before filter configuration back

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,4 +1,7 @@
 class CalendarController < ApplicationController
+  skip_before_filter :login_required, :only => [:show]
+  prepend_before_filter :login_or_feed_token_required, :only => [:show]
+
   def show
     @source_view = 'calendar'
     @page_title = t('todos.calendar_page_title')
