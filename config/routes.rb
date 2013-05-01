70fc1848c24d3fbc53165926c12d5f0a0c346317
Matt Rogers <mattrogers@sbcglobal.net>
2013-04-30 19:18:27 -0500

    Remove extra whitespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,17 @@ Tracksapp::Application.routes.draw do
   mount Tolk::Engine => '/tolk', :as => 'tolk' if Rails.env=='development'
 
   root :to => 'todos#index'
-  
+
   match 'login' => 'login#login'
   match 'login/expire_session' => 'login#expire_session'
   match 'login/check_expiry' => 'login#check_expiry'
   match 'logout' => 'login#logout'
-  
+
   match "tickler" => "todos#list_deferred"
   match 'review' => "projects#review"
   match 'calendar' => "todos#calendar"
   match 'done' => "stats#done", :as => 'done_overview'
-  
+
   match 'search' => 'search#index'
   match 'search/results' => 'search#results', :via => 'post'
 
@@ -21,7 +21,7 @@ Tracksapp::Application.routes.draw do
   match 'data/yaml_export' => 'data#yaml_export'
   match 'data/xml_export' => 'data#xml_export'
   match 'data/csv_actions' => 'data#csv_actions'
-  
+
   match 'integrations' => "integrations#index"
   match 'integrations/rest_api' => "integrations#rest_api", :as => 'rest_api_docs'
   match 'integrations/cloudmailin' => 'integrations#cloudmailin'
@@ -30,14 +30,14 @@ Tracksapp::Application.routes.draw do
   match 'integrations/get_applescript1.js' => 'integrations#get_applescript1'
   match 'integrations/get_applescript2.js' => 'integrations#get_applescript2'
   match 'integrations/get_quicksilver_applescript.js' => 'integrations#get_quicksilver_applescript'
-  
+
   match 'preferences' => "preferences#index"
   match 'preferences/render_date_format' => "preferences#render_date_format"
-  
+
   match 'feeds' => "feedlist#index", :as => 'feeds'
   match 'feedlist/get_feeds_for_context' => 'feedlist#get_feeds_for_context'
   match 'feedlist/get_feeds_for_project' => 'feedlist#get_feeds_for_project'
-  
+
   match 'stats' => 'stats#index'
   match 'stats/actions_done_last12months_data' => 'stats#actions_done_last12months_data'
   match 'stats/actions_done_last_years' => 'stats#actions_done_last_years'
@@ -54,7 +54,7 @@ Tracksapp::Application.routes.draw do
   match 'stats/actions_time_of_day_all_data' => 'stats#actions_time_of_day_all_data'
   match 'stats/actions_time_of_day_30days_data' => 'stats#actions_time_of_day_30days_data'
   match 'stats/show_selected_actions_from_chart/:id' => 'stats#show_selected_actions_from_chart', :as => 'show_actions_from_chart'
-  
+
   resources :contexts do
     member do
       get 'done_todos'
@@ -66,7 +66,7 @@ Tracksapp::Application.routes.draw do
     end
     resources :todos
   end
-  
+
   resources :projects do
     member do
       get 'done_todos'
@@ -81,7 +81,7 @@ Tracksapp::Application.routes.draw do
     end
     resources :todos
   end
-  
+
   resources :todos do
     member do
       put 'toggle_check'
@@ -119,7 +119,7 @@ Tracksapp::Application.routes.draw do
       get 'done'
     end
   end
-  
+
   resources :users do
     member do
       get 'change_password'
@@ -131,8 +131,8 @@ Tracksapp::Application.routes.draw do
     end
   end
   match 'signup' => "users#new"
-  
+
   resources :notes
   resources :preferences
-      
+
 end
