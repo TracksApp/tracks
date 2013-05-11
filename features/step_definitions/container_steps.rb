d1de2998deeefe7083d3ba11989933429161894d
Reinier Balt <lrbalt@gmail.com>
2013-05-11 13:47:33 +0200

    ignore: trying to find issue on failing scenario on Travis

--- features/step_definitions/container_steps.rb
+++ features/step_definitions/container_steps.rb
@@ -91,6 +91,7 @@ Then(/^I should (not see|see) "([^"]*)" in the (completed|done today|done this w
   id = 'completed_rest_of_month_container'  if container == 'done this month'
 
   css = "div##{id} div#line_todo_#{find_todo(todo_description).id}"
+  page.should have_css(css)
   check_css_visibility(visible, css)
 end
 
