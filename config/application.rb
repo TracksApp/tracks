75ab8364a23519156fb44558033fab4b9ba11af0
Reinier Balt <lrbalt@gmail.com>
2013-07-29 12:25:33 +0200

    change version to 3.0devel

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,7 @@ module Tracksapp
     config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '3.0'
     
     # add /app/assets/swfs to asset pipeline for charts
     config.assets.paths << Rails.root.join("app", "assets", "swfs")
