bae4b2a5b71f345b1c19ad7b329c027fc0903936
Matt Rogers <mattrogers@sbcglobal.net>
2013-10-13 09:39:48 -0500

    Require 'timecop' explicitly
    This works around a problem with bundler's autorequires when not using
    a standard Rails environment.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
+require 'timecop'
 
 class UserTest < ActiveSupport::TestCase
   fixtures :users, :preferences, :projects, :contexts, :todos, :recurring_todos
