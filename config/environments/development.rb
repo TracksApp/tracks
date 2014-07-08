95956fc6f74c10c640db8c38400c021a25ba42fb
Dan Rice <dan@zoombody.com>
2014-07-07 22:30:21 -0400

    Enable migration check in development environment
    This is the default configuration in Rails 4.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -19,6 +19,9 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
+
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
