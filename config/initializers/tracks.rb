ccf0b45f27586c50b89ba4ee5a511b503eab2ec8
Dan Rice <dan@zoombody.com>
2014-09-12 20:21:59 -0400

    Remove last remnants of old development bar

--- config/initializers/tracks.rb
+++ config/initializers/tracks.rb
@@ -1,6 +1,3 @@
-# changed in development.rb to show under_construction bar
-NOTIFY_BAR = "" unless defined?(NOTIFY_BAR)
-
 tracks_version='2.3devel'
 # comment out next two lines if you do not want (or can not) the date of the
 # last git commit in the footer
