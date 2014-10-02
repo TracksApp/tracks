f6195905411b8a4b74631d17064200a1967fa208
Dan Rice <dan@zoombody.com>
2014-10-02 11:53:34 -0400

    Version 2.3.rc1

--- config/initializers/tracks.rb
+++ config/initializers/tracks.rb
@@ -1,7 +1,7 @@
-tracks_version='2.3devel'
+tracks_version='2.3.rc1'
 # comment out next two lines if you do not want (or can not) the date of the
 # last git commit in the footer
-info=`git log --pretty=format:"%ai" -1`
-tracks_version=tracks_version + ' ('+info+')'
+#info=`git log --pretty=format:"%ai" -1`
+#tracks_version=tracks_version + ' ('+info+')'
 
 TRACKS_VERSION=tracks_version
