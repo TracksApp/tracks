4ce7c6bcd4adc4d48fe1b2765945d1dd563d0db3
Carsten Otto <carsten.otto@andrena.de>
2015-05-22 23:02:41 +0200

    Provide data needed for partial views
    The partials for context and project need to compute the
    context's/project's id, which is done based on @context/@project.
    Provide this data.
    
    Fixes #1836

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -425,6 +425,8 @@ class TodosController < ApplicationController
 
     @saved = @todo.save
 
+    provide_project_or_context_for_view
+
     # this is set after save and cleared after reload, so save it here
     @removed_predecessors = @todo.removed_predecessors
 
@@ -454,6 +456,15 @@ class TodosController < ApplicationController
           render :action => "edit", :format => :m
         end
       end
+    end
+  end
+
+  def provide_project_or_context_for_view
+    # see application_helper:source_view_key, used in shown partials
+    if source_view_is :project
+      @project = @todo.project
+    elsif source_view_is :context
+      @context = @todo.context
     end
   end
 
