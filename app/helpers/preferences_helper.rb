7652791451959a6433a1906a215d5600c4db30f3
Matt Rogers <codemattr@gmail.com>
2016-01-30 09:39:25 -0600

    Update the preferences pages to Bootstrap

--- app/helpers/preferences_helper.rb
+++ app/helpers/preferences_helper.rb
@@ -2,18 +2,16 @@ module PreferencesHelper
 
   def pref(model, pref_name, &block)
     s = content_tag(:label, Preference.human_attribute_name(pref_name), :for => model+pref_name)
-    s << "<br/>".html_safe
     s << yield
-    s << "<br/><br/>".html_safe
     s
   end
 
   def pref_with_select_field(model, pref_name, collection = [ [t('preferences.is_true'),true], [t('preferences.is_false'), false] ])
-    pref(model, pref_name) { select(model, pref_name, collection) }
+    pref(model, pref_name) { select(model, pref_name, collection, {}, class: "form-control") }
   end
 
   def pref_with_text_field(model, pref_name)
-    pref(model, pref_name) { text_field(model, pref_name) }
+    pref(model, pref_name) { text_field(model, pref_name, class: "form-control") }
   end
 
 end
