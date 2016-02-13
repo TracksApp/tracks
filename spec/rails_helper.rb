8390e9bd9ef4d85e4341a8757a56de73c0ff9ed5
Dan Rice <dan@zoombody.com>
2016-02-13 14:49:02 -0500

    Use existing test fixtures with RSpec until migration is complete

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,7 +28,7 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_path = "#{::Rails.root}/test/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
