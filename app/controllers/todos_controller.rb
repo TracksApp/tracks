d08608c755d44ab6922c3d103ad1be4dafe684a1
Matt Rogers <codemattr@gmail.com>
2019-04-11 14:38:56 -0500

    Remove the query code from TodosController
    It has been completely replaced with our new query object

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1316,39 +1316,7 @@ end
   end
 
   def get_not_done_todos
-    if params[:done]
-      not_done_todos = current_user.todos.completed.completed_after(Time.zone.now - params[:done].to_i.days)
-    else
-      not_done_todos = current_user.todos.active.not_hidden
-    end
-
-    not_done_todos = not_done_todos.
-      reorder(Arel.sql("todos.due IS NULL, todos.due ASC, todos.created_at ASC")).
-      includes(Todo::DEFAULT_INCLUDES)
-
-    not_done_todos = not_done_todos.limit(sanitize(params[:limit])) if params[:limit]
-
-    if params[:due]
-      due_within_when = Time.zone.now + params['due'].to_i.days
-      not_done_todos = not_done_todos.where('todos.due <= ?', due_within_when)
-    end
-
-    if params[:tag]
-      tag = Tag.where(:name => params['tag']).first
-      not_done_todos = not_done_todos.where('taggings.tag_id = ?', tag.id)
-    end
-
-    if params[:context_id]
-      context = current_user.contexts.find(params[:context_id])
-      not_done_todos = not_done_todos.where('context_id' => context.id)
-    end
-
-    if params[:project_id]
-      project = current_user.projects.find(params[:project_id])
-      not_done_todos = not_done_todos.where('project_id' => project)
-    end
-
-    return Todos::UndoneTodosQuery.new(current_user).query(params)
+    Todos::UndoneTodosQuery.new(current_user).query(params)
   end
 
   def onsite_redirect_to(destination)
