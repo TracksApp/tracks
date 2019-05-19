f873a93eb37859fa605b566f33e4c74e7ba1013f
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2019-05-19 13:04:33 +0300

    #1153: Convert rest of the stats to use ChartJS

--- app/models/stats/actions.rb
+++ app/models/stats/actions.rb
@@ -34,23 +34,99 @@ module Stats
       @sum_actions_created_last12months ||= new_since(one_year)
     end
 
-    def completion_charts
-      @completion_charts ||= %w{
-     actions_done_last30days_data
-     actions_done_last12months_data
-     actions_completion_time_data
-      }.map do |action|
-        Stats::Chart.new(action)
-      end
+    def done_last12months_data
+      # get actions created and completed in the past 12+3 months. +3 for running
+      # - outermost set of entries needed for these calculations
+      actions_last12months = @user.todos.created_or_completed_after(@cut_off_year_plus3).select("completed_at,created_at")
+  
+      # convert to array and fill in non-existing months
+      @actions_done_last12months_array = put_events_into_month_buckets(actions_last12months, 13, :completed_at)
+      @actions_created_last12months_array = put_events_into_month_buckets(actions_last12months, 13, :created_at)
+  
+      # find max for graph in both arrays
+      @max = (@actions_done_last12months_array + @actions_created_last12months_array).max
+  
+      # find running avg
+      done_in_last_15_months = put_events_into_month_buckets(actions_last12months, 16, :completed_at)
+      created_in_last_15_months = put_events_into_month_buckets(actions_last12months, 16, :created_at)
+  
+      @actions_done_avg_last12months_array = compute_running_avg_array(done_in_last_15_months, 13)
+      @actions_created_avg_last12months_array = compute_running_avg_array(created_in_last_15_months, 13)
+  
+      # interpolate avg for current month.
+      @interpolated_actions_created_this_month = interpolate_avg_for_current_month(@actions_created_last12months_array)
+      @interpolated_actions_done_this_month = interpolate_avg_for_current_month(@actions_done_last12months_array)
+  
+      @created_count_array = Array.new(13, actions_last12months.created_after(@cut_off_year).count(:all)/12.0)
+      @done_count_array    = Array.new(13, actions_last12months.completed_after(@cut_off_year).count(:all)/12.0)
+
+      return {
+	datasets: [
+	  {label: "Avg created", data: @created_count_array.map { |total| [total] }, type: "line"},
+	  {label: "Avg completed", data: @done_count_array.map { |total| [total] }, type: "line"},
+	  {label: "3 months avg completed", data: @actions_done_avg_last12months_array.map { |total| [total] }, type: "line"},
+	  {label: "3 months avg created", data: @actions_created_avg_last12months_array.map { |total| [total] }, type: "line"},
+	  {label: "Created", data: @actions_created_last12months_array.map { |total| [total] } },
+	  {label: "Completed", data: @actions_done_last12months_array.map { |total| [total] } }
+	],
+	labels: @actions_done_avg_last12months_array.each_with_index.map { |total, month| [month] }
+      }
     end
 
-    def timing_charts
-      @timing_charts ||= %w{
-      actions_visible_running_time_data
-      actions_running_time_data
-      }.map do |action|
-        Stats::Chart.new(action)
-      end
+    def done_last30days_data
+      # get actions created and completed in the past 30 days.
+      @actions_done_last30days = @user.todos.completed_after(@cut_off_30days).select("completed_at")
+      @actions_created_last30days = @user.todos.created_after(@cut_off_30days).select("created_at")
+  
+      # convert to array. 30+1 to have 30 complete days and one current day [0]
+      @actions_done_last30days_array = convert_to_days_from_today_array(@actions_done_last30days, 31, :completed_at)
+      @actions_created_last30days_array = convert_to_days_from_today_array(@actions_created_last30days, 31, :created_at)
+
+      # find max for graph in both hashes
+      @max = [@actions_done_last30days_array.max, @actions_created_last30days_array.max].max
+
+      created_count_array = Array.new(30){ |i| @actions_created_last30days.size/30.0 }
+      done_count_array    = Array.new(30){ |i| @actions_done_last30days.size/30.0 }
+      # TODO: make the strftime i18n proof
+      # TODO: Fix this, broke during transition from Flash-based stats.
+#      time_labels         = Array.new(30){ |i| l(Time.zone.now-i.days, :format => :stats)  }
+  
+      return {
+	datasets: [
+	  {label: "Avg created", data: created_count_array.map { |total| [total] }, type: "line"},
+	  {label: "Avg completed", data: done_count_array.map { |total| [total] }, type: "line"},
+	  {label: "Created", data: @actions_created_last30days_array.map { |total| [total] } },
+	  {label: "Completed", data: @actions_done_last30days_array.map { |total| [total] } }
+	],
+#	labels: time_labels
+	labels: @actions_done_last30days_array.each_with_index.map { |total, days| [days] }
+      }
+    end
+
+    def completion_time_data
+      @actions_completion_time = @user.todos.completed.select("completed_at, created_at").reorder("completed_at DESC" )
+  
+      # convert to array and fill in non-existing weeks with 0
+      @max_weeks = @actions_completion_time.last ? difference_in_weeks(@today, @actions_completion_time.last.completed_at) : 1
+      @actions_completed_per_week_array = convert_to_weeks_running_array(@actions_completion_time, @max_weeks+1)
+  
+      # stop the chart after 10 weeks
+      @count = [10, @max_weeks].min
+  
+      # convert to new array to hold max @cut_off elems + 1 for sum of actions after @cut_off
+      @actions_completion_time_array = cut_off_array_with_sum(@actions_completed_per_week_array, @count)
+      @max_actions = @actions_completion_time_array.max
+  
+      # get percentage done cumulative
+      @cum_percent_done = convert_to_cumulative_array(@actions_completion_time_array, @actions_completion_time.count(:all))
+  
+      return {
+	datasets: [
+	  {label: "Percentage", data: @cum_percent_done.map { |total| [total] }, type: "line"},
+	  {label: "Actions", data: @actions_completion_time_array.map { |total| [total] } }
+	],
+	labels: @actions_completion_time_array.each_with_index.map { |total, week| [week] }
+      }
     end
 
     def running_time_data
@@ -70,10 +146,48 @@ module Stats
       # get percentage done cumulative
       @cum_percent_done = convert_to_cumulative_array(@actions_running_time_array, @actions_running_time.count )
 
-      return [
-        {name: "Percentage", data: @cum_percent_done.each_with_index.map { |total, week| [week, total] } , type: "line"},
-	{name: "Actions", data: @actions_running_time_array.each_with_index.map { |total, week| [week, total] } }
-      ]
+      return {
+	datasets: [
+	  {label: "Percentage", data: @cum_percent_done.map { |total| [total] }, type: "line"},
+	  {label: "Actions", data: @actions_running_time_array.map { |total| [total] } }
+	],
+	labels: @actions_running_time_array.each_with_index.map { |total, week| [week] }
+      }
+    end
+
+    def visible_running_time_data
+      # running means
+      # - not completed (completed_at must be null)
+      # visible means
+      # - actions not part of a hidden project
+      # - actions not part of a hidden context
+      # - actions not deferred (show_from must be null)
+      # - actions not pending/blocked
+  
+      @actions_running_time = @user.todos.not_completed.not_hidden.not_deferred_or_blocked.
+        select("todos.created_at").
+        reorder("todos.created_at DESC")
+  
+      @max_weeks = difference_in_weeks(@today, @actions_running_time.last.created_at)
+      @actions_running_per_week_array = convert_to_weeks_from_today_array(@actions_running_time, @max_weeks+1, :created_at)
+  
+      # cut off chart at 52 weeks = one year
+      @count = [52, @max_weeks].min
+  
+      # convert to new array to hold max @cut_off elems + 1 for sum of actions after @cut_off
+      @actions_running_time_array = cut_off_array_with_sum(@actions_running_per_week_array, @count)
+      @max_actions = @actions_running_time_array.max
+  
+      # get percentage done cumulative
+      @cum_percent_done = convert_to_cumulative_array(@actions_running_time_array, @actions_running_time.count )
+  
+      return {
+	datasets: [
+	  {label: "Percentage", data: @cum_percent_done.map { |total| [total] }, type: "line"},
+	  {label: "Actions", data: @actions_running_time_array.map { |total| [total] } }
+	],
+	labels: @actions_running_time_array.each_with_index.map { |total, week| [week] }
+      }
     end
 
     def open_per_week_data
@@ -89,7 +203,12 @@ module Stats
       @actions_open_per_week_array = convert_to_weeks_running_from_today_array(@actions_started, @max_weeks+1)
       @actions_open_per_week_array = cut_off_array(@actions_open_per_week_array, @count)
 
-      return @actions_open_per_week_array.each_with_index.map { |total, week| [week, total] }
+      return {
+	datasets: [
+	  {label: "Actions", data: @actions_open_per_week_array.map { |total| [total] } }
+	],
+	labels: @actions_open_per_week_array.each_with_index.map { |total, week| [week] }
+      }
     end
 
     def day_of_week_all_data
@@ -202,14 +321,12 @@ module Stats
       @completed ||= user.todos.completed.select("completed_at, created_at")
     end
 
-    # assumes date1 > date2
-    def difference_in_days(date1, date2)
-      return ((date1.utc.at_midnight-date2.utc.at_midnight)/SECONDS_PER_DAY).to_i
+    def interpolate_avg_for_current_month(set)
+      (set[0]*(1/percent_of_month) + set[1] + set[2]) / 3.0
     end
-  
-    # assumes date1 > date2
-    def difference_in_weeks(date1, date2)
-      return difference_in_days(date1, date2) / 7
+
+    def percent_of_month
+      Time.zone.now.day / Time.zone.now.end_of_month.day.to_f
     end
 
     # uses the supplied block to determine array of indexes in hash
@@ -220,12 +337,24 @@ module Stats
       a
     end
   
-    def convert_to_weeks_running_from_today_array(records, array_size)
-      return convert_to_array(records, array_size) { |r| week_indexes_of(r) }
+    def put_events_into_month_buckets(records, array_size, date_method_on_todo)
+      convert_to_array(records.select { |x| x.send(date_method_on_todo) }, array_size) { |r| [difference_in_months(@today, r.send(date_method_on_todo))]}
     end
 
-    def cut_off_array(array, cut_off)
-      return Array.new(cut_off){|i| array[i]||0}
+    def convert_to_days_from_today_array(records, array_size, date_method_on_todo)
+      return convert_to_array(records, array_size){ |r| [difference_in_days(@today, r.send(date_method_on_todo))]}
+    end
+
+    def convert_to_weeks_from_today_array(records, array_size, date_method_on_todo)
+      return convert_to_array(records, array_size) { |r| [difference_in_weeks(@today, r.send(date_method_on_todo))]}
+    end
+
+    def convert_to_weeks_running_array(records, array_size)
+      return convert_to_array(records, array_size) { |r| [difference_in_weeks(r.completed_at, r.created_at)]}
+    end
+
+    def convert_to_weeks_running_from_today_array(records, array_size)
+      return convert_to_array(records, array_size) { |r| week_indexes_of(r) }
     end
 
     def week_indexes_of(record)
@@ -236,16 +365,16 @@ module Stats
       return a
     end
 
-    def convert_to_weeks_from_today_array(records, array_size, date_method_on_todo)
-      return convert_to_array(records, array_size) { |r| [difference_in_weeks(@today, r.send(date_method_on_todo))]}
-    end
-
     def cut_off_array_with_sum(array, cut_off)
       # +1 to hold sum of rest
       a = Array.new(cut_off+1){|i| array[i]||0}
       # add rest of array to last elem
       a[cut_off] += array.inject(:+) - a.inject(:+)
       return a
+    end
+
+    def cut_off_array(array, cut_off)
+      return Array.new(cut_off){|i| array[i]||0}
     end
 
     def convert_to_cumulative_array(array, max)
@@ -256,5 +385,34 @@ module Stats
       return a
     end
 
+    def difference_in_months(date1, date2)
+      return (date1.utc.year - date2.utc.year)*12 + (date1.utc.month - date2.utc.month)
+    end
+
+    # assumes date1 > date2
+    def difference_in_days(date1, date2)
+      return ((date1.utc.at_midnight-date2.utc.at_midnight)/SECONDS_PER_DAY).to_i
+    end
+  
+    # assumes date1 > date2
+    def difference_in_weeks(date1, date2)
+      return difference_in_days(date1, date2) / 7
+    end
+
+    def three_month_avg(set, i)
+      (set.fetch(i){ 0 } + set.fetch(i+1){ 0 } + set.fetch(i+2){ 0 }) / 3.0
+    end
+
+    def set_three_month_avg(set,upper_bound)
+      (0..upper_bound-1).map { |i| three_month_avg(set, i) }
+    end
+
+    def compute_running_avg_array(set, upper_bound)
+      result = set_three_month_avg(set, upper_bound)
+      result[upper_bound-1] = result[upper_bound-1] * 3 if upper_bound == set.length
+      result[upper_bound-2] = result[upper_bound-2] * 3 / 2 if upper_bound > 1 and upper_bound == set.length
+      result[0] = "null"
+      result
+    end # unsolved, not triggered, edge case for set.length == upper_bound + 1
   end
 end
