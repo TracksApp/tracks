22530a6b80198a95eff717cf12d8f3f429be404c
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2019-05-22 03:36:27 +0300

    Fix the preferences form: Labels' for attributes, boolean fields and autofill of the new password field

--- app/helpers/preferences_helper.rb
+++ app/helpers/preferences_helper.rb
@@ -1,9 +1,13 @@
 module PreferencesHelper
 
   def pref(model, pref_name, &block)
-    s = content_tag(:label, Preference.human_attribute_name(pref_name), :for => model+pref_name)
+    s = content_tag(:label, Preference.human_attribute_name(pref_name), :for => model + "_" + pref_name)
     s << yield
     s
+  end
+
+  def pref_with_check_box_field(model, pref_name)
+    pref(model, pref_name) { check_box(model, pref_name, class: "form-control") }
   end
 
   def pref_with_select_field(model, pref_name, collection = [ [t('preferences.is_true'),true], [t('preferences.is_false'), false] ])
