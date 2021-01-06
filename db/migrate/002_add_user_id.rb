83dd49a52fd11afd24a8a1820ca59c52b5d627d5
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2021-01-06 15:33:13 +0200

    Small fix to fi locale and whitespace changes to migrate script

--- db/migrate/002_add_user_id.rb
+++ db/migrate/002_add_user_id.rb
@@ -1,12 +1,12 @@
 class AddUserId < ActiveRecord::Migration[5.2]
-  
+
   class Project < ActiveRecord::Base; end
   class Context < ActiveRecord::Base; end
   class Todo < ActiveRecord::Base; end
-  
+
   def self.up
     add_column :contexts, :user_id, :integer, :default => 1
-    add_column :projects, :user_id, :integer, :default => 1 
+    add_column :projects, :user_id, :integer, :default => 1
     add_column :todos, :user_id, :integer, :default => 1
     Context.all.each { |context| context.user_id = 1 }
     Project.all.each { |project| project.user_id = 1 }
