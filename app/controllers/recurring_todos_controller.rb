TracksApp/tracks

commit a7d8b3c95008b185346d4007f9e25cf1c8088371

Use exists? instead of count > 0

app/controllers/recurring_todos_controller.rb
@@ -12,8 +12,8 @@ class RecurringTodosController < ApplicationController
     @recurring_todos = current_user.recurring_todos.active.includes(:tags, :taggings)
     @completed_recurring_todos = current_user.recurring_todos.completed.limit(10).includes(:tags, :taggings)
 
-    @no_recurring_todos = @recurring_todos.count == 0
-    @no_completed_recurring_todos = @completed_recurring_todos.count == 0
+    @no_recurring_todos = @recurring_todos.exists?
+    @no_completed_recurring_todos = @completed_recurring_todos.exists?
     @count = @recurring_todos.count
 
     @new_recurring_todo = RecurringTodo.new

commit 06de71c5bbd343c833f6c93d83b1b0a22a27ff62

Fix the logic for no recurring events label broken in a7d8b3c95.

app/controllers/recurring_todos_controller.rb
@@ -11,8 +11,8 @@ class RecurringTodosController < ApplicationController
     @recurring_todos = current_user.recurring_todos.active.includes(:tags, :taggings)
     @completed_recurring_todos = current_user.recurring_todos.completed.limit(10).includes(:tags, :taggings)
 
-    @no_recurring_todos = @recurring_todos.exists?
-    @no_completed_recurring_todos = @completed_recurring_todos.exists?
+    @no_recurring_todos = !@recurring_todos.exists?
+    @no_completed_recurring_todos = !@completed_recurring_todos.exists?
     @count = @recurring_todos.count
 
     @new_recurring_todo = RecurringTodo.new