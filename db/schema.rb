36f139d8793c049cfa48ea27fb04dbea7f35e645
Jyri-Petteri Paloposki <jyri-petteri.paloposki@iki.fi>
2024-07-19 14:00:19 +0300

    Update schema.rb for Postgres support

--- db/schema.rb
+++ db/schema.rb
@@ -2,30 +2,33 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_08_20_000743) do
 
-  create_table "attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "todo_id"
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "attachments", force: :cascade do |t|
+    t.bigint "todo_id"
     t.string "file_file_name"
     t.string "file_content_type"
-    t.integer "file_file_size"
+    t.bigint "file_file_size"
     t.datetime "file_updated_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["todo_id"], name: "index_attachments_on_todo_id"
   end
 
-  create_table "contexts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "contexts", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "position", default: 0
+    t.integer "position"
     t.integer "user_id", default: 1
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -34,7 +37,7 @@ ActiveRecord::Schema.define(version: 2020_08_20_000743) do
     t.index ["user_id"], name: "index_contexts_on_user_id"
   end
 
-  create_table "dependencies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "dependencies", force: :cascade do |t|
     t.integer "successor_id", null: false
     t.integer "predecessor_id", null: false
     t.string "relationship_type"
@@ -42,7 +45,7 @@ ActiveRecord::Schema.define(version: 2020_08_20_000743) do
     t.index ["successor_id"], name: "index_dependencies_on_successor_id"
   end
 
-  create_table "notes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "notes", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "project_id", null: false
     t.text "body"
@@ -52,7 +55,7 @@ ActiveRecord::Schema.define(version: 2020_08_20_000743) do
     t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
-  create_table "open_id_authentication_associations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "open_id_authentication_associations", force: :cascade do |t|
     t.integer "issued"
     t.integer "lifetime"
     t.string "handle"
@@ -61,13 +64,13 @@ ActiveRecord::Schema.define(version: 2020_08_20_000743) do
     t.binary "secret"
   end
 
-  create_table "open_id_authentication_nonces", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "open_id_authentication_nonces", force: :cascade do |t|
     t.integer "timestamp", null: false
     t.string "server_url"
     t.string "salt", null: false
   end
 
-  create_table "preferences", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "preferences", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "date_format", limit: 40, default: "%d/%m/%Y", null: false
     t.integer "week_starts", default: 0, null: false
@@ -79,9 +82,9 @@ ActiveRecord::Schema.define(version: 2020_08_20_000743) do
     t.integer "refresh", default: 0, null: false
     t.boolean "verbose_action_descriptors", default: false, null: false
     t.boolean "show_hidden_projects_in_sidebar", default: true, null: false
-    t.string "time_zone", default: "London", null: false
+    t.string "time_zone", limit: 255, default: "London", null: false
     t.boolean "show_project_on_todo_done", default: false, null: false
-    t.string "title_date_format", default: "%A, %d %B %Y", null: false
+    t.string "title_date_format", limit: 255, default: "%A, %d %B %Y", null: false
     t.integer "mobile_todos_per_page", default: 6, null: false
     t.string "sms_email"
     t.integer "sms_context_id"
@@ -91,11 +94,11 @@ ActiveRecord::Schema.define(version: 2020_08_20_000743) do
     t.index ["user_id"], name: "index_preferences_on_user_id"
   end
 
-  create_table "projects", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "projects", force: :cascade do |t|
     t.string "name", null: false
-    t.integer "position", default: 0
+    t.integer "position"
     t.integer "user_id", default: 1
-    t.text "description", limit: 16777215
+    t.text "description"
     t.string "state", limit: 20, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -109,12 +112,12 @@ ActiveRecord::Schema.define(version: 2020_08_20_000743) do
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
-  create_table "recurring_todos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "recurring_todos", force: :cascade do |t|
     t.integer "user_id", default: 1
     t.integer "context_id", null: false
     t.integer "project_id"
     t.string "description", null: false
-    t.text "notes", limit: 16777215
+    t.text "notes"
     t.string "state", limit: 20, null: false
     t.datetime "start_from"
     t.string "ends_on"
@@ -133,21 +136,21 @@ ActiveRecord::Schema.define(version: 2020_08_20_000743) do
     t.integer "every_count"
     t.integer "weekday"
     t.datetime "completed_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean "show_always"
     t.index ["state"], name: "index_recurring_todos_on_state"
     t.index ["user_id"], name: "index_recurring_todos_on_user_id"
   end
 
-  create_table "sessions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "sessions", force: :cascade do |t|
     t.string "session_id"
     t.text "data"
     t.datetime "updated_at"
-    t.index ["session_id"], name: "sessions_session_id_index"
+    t.index ["session_id"], name: "index_sessions_on_session_id"
   end
 
-  create_table "taggings", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "taggings", force: :cascade do |t|
     t.integer "taggable_id"
     t.integer "tag_id"
     t.string "taggable_type"
@@ -156,7 +159,7 @@ ActiveRecord::Schema.define(version: 2020_08_20_000743) do
     t.index ["taggable_id", "taggable_type"], name: "index_taggings_on_taggable_id_and_taggable_type"
   end
 
-  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "tags", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -164,11 +167,11 @@ ActiveRecord::Schema.define(version: 2020_08_20_000743) do
     t.index ["name"], name: "index_tags_on_name"
   end
 
-  create_table "todos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "todos", force: :cascade do |t|
     t.integer "context_id", null: false
     t.integer "project_id"
     t.text "description", null: false
-    t.text "notes", limit: 16777215
+    t.text "notes"
     t.datetime "created_at"
     t.datetime "due"
     t.datetime "completed_at"
@@ -177,7 +180,6 @@ ActiveRecord::Schema.define(version: 2020_08_20_000743) do
     t.string "state", limit: 20, null: false
     t.integer "recurring_todo_id"
     t.datetime "updated_at"
-    t.text "rendered_notes", limit: 16777215
     t.index ["context_id"], name: "index_todos_on_context_id"
     t.index ["project_id"], name: "index_todos_on_project_id"
     t.index ["state"], name: "index_todos_on_state"
@@ -186,20 +188,20 @@ ActiveRecord::Schema.define(version: 2020_08_20_000743) do
     t.index ["user_id", "state"], name: "index_todos_on_user_id_and_state"
   end
 
-  create_table "tolk_locales", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "tolk_locales", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["name"], name: "index_tolk_locales_on_name", unique: true
   end
 
-  create_table "tolk_phrases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "tolk_phrases", force: :cascade do |t|
     t.text "key"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "tolk_translations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "tolk_translations", force: :cascade do |t|
     t.integer "phrase_id"
     t.integer "locale_id"
     t.text "text"
@@ -210,9 +212,9 @@ ActiveRecord::Schema.define(version: 2020_08_20_000743) do
     t.index ["phrase_id", "locale_id"], name: "index_tolk_translations_on_phrase_id_and_locale_id", unique: true
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "login", limit: 80, null: false
-    t.string "crypted_password", limit: 60
+    t.string "crypted_password", limit: 60, null: false
     t.string "token"
     t.boolean "is_admin", default: false, null: false
     t.string "first_name"
